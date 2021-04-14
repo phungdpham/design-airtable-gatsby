b5d29f6703f4637411ff5a5ac8f5fa13bea8dd27
phungdpham <phung.tech@gmail.com>
2021-04-14 02:00:53 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from "../components"
+import SEO from "../components/seo"
+const HomePage = ({ data }) => {
+  const {allAirtable: {nodes:projects}} = data
+  return (
+    <Layout>
+      <Hero />
+      <About />
+      <Projects projects={projects} title="latest projects"/>
+      <Slider />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 3
+      sort: { fields: data___date, order: DESC }
+    ) {
+      nodes {
+        id
+        data {
+          name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default HomePage
